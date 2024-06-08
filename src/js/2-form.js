aceb0e6f75fee7badc6c24a34290585906076c1a
Illek <dvrheim@gmail.com>
2024-06-09 00:27:49 +0300

    Fix mistakes
    Push it! Push it!

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,7 +6,6 @@ const formData = {
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('.feedback-form > label > input');
 const message = document.querySelector('.feedback-form > label > textarea');
-const button = form.children[2];
 
 form.addEventListener('input', () => {
   formData.email = email.value.trim();
@@ -16,7 +15,7 @@ form.addEventListener('input', () => {
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  if (email.value === '' || message.value === '') {
+  if (formData.email === '' || formData.message === '') {
     alert("Don't forget to fill in all required fields!");
   } else {
     console.log(formData);
@@ -31,6 +30,8 @@ const formContent = () => {
     const data = JSON.parse(savedData);
     email.value = data.email;
     message.value = data.message;
+    formData.email = data.email;
+    formData.message = data.message;
   }
 };
 
